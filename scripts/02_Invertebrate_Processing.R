96bf9500536f27dae50e0563184ae1e48444d30d
Frankie Gerraty <frankiegerraty@gmail.com>
2024-01-03 20:27:06 -0800

    Create non-linear models and functions for predicting mass from length for TRTR and ISOPOD

--- scripts/02_Invertebrate_Processing.R
+++ scripts/02_Invertebrate_Processing.R
@@ -10,38 +10,44 @@
 invertebrates <- read_csv("data/processed/invertebrates.csv")
 
 
-
-#Create TRTR Model
+#Create TRTR Exponential growth curve model. 
 
 TRTR <- invertebrates %>% 
   filter(species_id == "TRTR") %>% 
   drop_na(length, mass)
 
-ggplot(TRTR, aes(x=length, y=mass))+
-  geom_point()
+TRTR_model <- nls(mass ~ I(a + b * exp(c * length)), 
+                  data = TRTR, 
+                  start = list(a = 1, b = 1, c = 1), trace = T)
 
-ggplot(TRTR, aes(x=log(length), y=log(mass)))+
-  geom_point()
+summary(TRTR_model)
 
-
-#Fit an exponential curve model. 
-#Exponential 3P provides best fitting model in JMP
-#nlme package? 
-
-
-
-#Create ISOPOD Model
+#Create function that pulls coefficient estimate values from TRTR_model to predict the mass of TRTR amphipods.   
+predict_TRTR_mass <- function(length)
+{summary(TRTR_model)$coefficients[1,1] + #"a" estimate from model
+    summary(TRTR_model)$coefficients[2,1] * #"b" estimate from model
+    exp(summary(TRTR_model)$coefficients[3,1] * length )} #exp(c * length) 
 
 
+
+
+#Select observations with isopods that were both measured (length) and massed (mass)
 ISOPOD <- invertebrates %>% 
   filter(species_id == "ISOPOD") %>% 
   drop_na(length, mass)
 
-ggplot(ISOPOD, aes(x= length, y= mass)) +
-  geom_point()
+#Create non-linear model 
+ISOPOD_model <- nls(mass ~ I(a + b * exp(c * length)), 
+                    data = ISOPOD, 
+                    start = list(a = 1, b = 1, c = 1), trace = T)
 
-ggplot(ISOPOD, aes(x= log(length), y= log(mass))) +
-  geom_point()
+#Create function that pulls coefficient estimate values from ISOPOD_model to predict the mass of isopods.  
+predict_ISOPOD_mass <- function(length)
+  {summary(ISOPOD_model)$coefficients[1,1] + #"a" estimate from model
+    summary(ISOPOD_model)$coefficients[2,1] * #"b" estimate from model
+    exp(summary(ISOPOD_model)$coefficients[3,1] * length )} #exp(c * length) 
 
 
-#Use projected mass for 
+
+invertebrates2 <- invertebrates %>% 
+  mutate(project_mass = predict_ISOPOD_mass(length))
